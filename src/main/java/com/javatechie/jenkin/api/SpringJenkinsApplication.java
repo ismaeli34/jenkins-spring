c41a2cd588d2286f8e6520c433381d59193f6a42
ronneyismael <ronneyismael@gmail.com>
2023-02-27 17:40:57 +0100

    first commit

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -4,16 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 @RestController
 public class SpringJenkinsApplication {
 
+ Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
-	@GetMapping("/ping")
-	public String message() {
-		return "Wao!! Application Deployed successfully in SAP Cloud..";
-	}
+ public void init(){
+	logger.info("Application started...");
+ }
+
+
+
+	// @GetMapping("/ping")
+	// public String message() {
+	// 	return "Wao!! Application Deployed successfully in SAP Cloud..";
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
